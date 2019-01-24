a1f930dd26cbf9818771cbab0bc9e092d3341afe
Prachaya Ruangprach <36035081+adfbjsdknfsdfnksdnfsdnf@users.noreply.github.com>
2019-01-24 15:52:31 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'b+/rHcPbqhi0nobGrSc2GFwJwjiDkT75ypwUeZqVmnlMAo5BB3TckEiNuESWAjIcr+j4s7sGoL/6SLW8F7WTZ6URQicyQ4LEzfOOy9OFNLvhUDOof2YjCIHREOn5qsnT7tSnPRuhMxt761gn+rjKgQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
